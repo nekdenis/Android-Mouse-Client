f0a994278fc8ee36175785a689b641003065b096
nekdenis <batman36@mail.ru>
2013-12-01 16:40:53 +0400

    refactored

--- src/com/prashant/adesara/socket/client/MyActivity.java
+++ src/com/prashant/adesara/socket/client/MyActivity.java
@@ -1,122 +1,132 @@
 package com.prashant.adesara.socket.client;
 
-import java.util.ArrayList;
-
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
-import android.hardware.SensorListener;
 import android.hardware.SensorManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ListView;
 
-import com.prashant.custom.adapter.MyCustomAdapter;
-
-/**
- * @author Prashant Adesara
- * Display Activity with sending messages to server 
- * */
-
-@SuppressLint("NewApi")
 public class MyActivity extends Activity
 {
-    private ListView mList;
-    private ArrayList<String> arrayList;
-    private MyCustomAdapter mAdapter;
-    private TCPClient mTcpClient = null;
+    private TCPClient tcpClient = null;
     private connectTask conctTask = null;
-    private SensorManager mSensorManager;
-    private Sensor mSensor;
-    @SuppressWarnings("deprecation")
-	@Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-        
-    	super.onCreate(savedInstanceState);
+    private SensorManager sensorManager;
+    private Sensor sensor;
+
+    private Button leftButton;
+    private Button rightButton;
+    private Button connectButton;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        arrayList = new ArrayList<String>();
- 
-        final EditText editText = (EditText) findViewById(R.id.editText);
-        Button send = (Button)findViewById(R.id.send_button);
- 
-        //relate the listView from java to the one created in xml
-        mList = (ListView)findViewById(R.id.list);
-        mAdapter = new MyCustomAdapter(this, arrayList);
-        mList.setAdapter(mAdapter);
- 
-        mTcpClient = null;
-        // connect to the server
-        conctTask = new connectTask();
-        conctTask.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
-        
-        send.setOnClickListener(new View.OnClickListener() {
+        initView();
+        initListeners();
+        initSensor();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        initConnection();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        closeConnection();
+    }
+
+    private void initView(){
+        leftButton = (Button) findViewById(R.id.main_left_button);
+        rightButton = (Button) findViewById(R.id.main_right_button);
+        connectButton = (Button) findViewById(R.id.main_connect_button);
+    }
+
+    private void initListeners() {
+        leftButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                String message = editText.getText().toString();
-                //add the text in the arrayList
-                arrayList.add("Android Client: " + message);
-                //sends the message to the server
-                if (mTcpClient != null) 
-                {
-                    mTcpClient.sendMessage("Android Client: "+message);
-                }
-                //refresh the list
-                mAdapter.notifyDataSetChanged();
-                editText.setText("");
+            public void onClick(View v) {
+
             }
         });
+        rightButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY);
-        mSensorManager.registerListener(new SensorEventListener() {
-        	float[] history = {0f,0f};
-			@Override
-			public void onSensorChanged(SensorEvent event) {
-				
-                if (mTcpClient != null) 
-                {	
-                		mTcpClient.sendMessage("x"+event.values[0]+"y"+event.values[1]);
-                }
-			}
-			
-			@Override
-			public void onAccuracyChanged(Sensor sensor, int accuracy) {
-				// TODO Auto-generated method stub
-				
-			}
-		}, mSensor, SensorManager.SENSOR_DELAY_GAME);
+            }
+        });
+        connectButton.setOnClickListener(new ConnectButtonListener());
     }
-    
-    /**
-     * @author Prashant Adesara
-     * receive the message from server with asyncTask  
-     * */
+
+    @SuppressLint("NewApi")
+    private void initConnection() {
+        conctTask = new connectTask();
+        conctTask.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+    }
+
+    private void closeConnection() {
+        try
+        {
+            tcpClient.sendMessage("bye");
+            tcpClient.stopClient();
+            conctTask.cancel(true);
+            conctTask = null;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        setDisconnected();
+    }
+
+    private void setDisconnected() {
+        connectButton.setText(R.string.main_disconnect);
+        connectButton.setOnClickListener(new DisconnectButtonListener());
+
+    }
+
+    private void setConnected() {
+        connectButton.setText(R.string.main_reconnect);
+        connectButton.setOnClickListener(new ConnectButtonListener());
+    }
+
+
+    private void initSensor() {
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY);
+        sensorManager.registerListener(new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent event) {
+
+                if (tcpClient != null) {
+                    tcpClient.sendMessage("move_"+"x" + event.values[0] + "y" + event.values[1]);
+                }
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int accuracy) {
+            }
+        }, sensor, SensorManager.SENSOR_DELAY_GAME);
+    }
+
     public class connectTask extends AsyncTask<String,String,TCPClient> {
         @Override
         protected TCPClient doInBackground(String... message) 
         {
-            //we create a TCPClient object and
-            mTcpClient = new TCPClient(new TCPClient.OnMessageReceived() 
-            {
+            tcpClient = new TCPClient(new TCPClient.OnMessageReceived() {
+
                 @Override
-                //here the messageReceived method is implemented
-                public void messageReceived(String message) 
+                public void messageReceived(String message)
                 {
                 	try
 					{
-                		//this method calls the onProgressUpdate
                 		publishProgress(message);
-                		if(message!=null)
-                		{
-                			System.out.println("Return Message from Socket::::: >>>>> "+message);
-                		}
 					}
 					catch (Exception e)
 					{
@@ -124,10 +134,13 @@ public class MyActivity extends Activity
 					}
                 }
             });
-            mTcpClient.run();
-            if(mTcpClient!=null)
-            {
-            	mTcpClient.sendMessage("Initial Message when connected with Socket Server");
+            tcpClient.run();
+            if (tcpClient != null) {
+                publishProgress("connected");
+                tcpClient.sendMessage("Hello!");
+            } else {
+                publishProgress("disconnected");
+
             }
             return null;
         }
@@ -135,33 +148,27 @@ public class MyActivity extends Activity
         @Override
         protected void onProgressUpdate(String... values) {
             super.onProgressUpdate(values);
- 
-            //in the arrayList we add the messaged received from server
-            arrayList.add(values[0]);
-            
-            // notify the adapter that the data set has changed. This means that new message received
-            // from server was added to the list
-            mAdapter.notifyDataSetChanged();
+            if(((String)values[0]).equals("connected")){
+                setConnected();
+            }else if(((String)values[0]).equals("disconnected")){
+                setDisconnected();
+            }
         }
     }
 
-    @Override
-    protected void onDestroy()
-    {
-    	try
-		{
-    		System.out.println("onDestroy.");
-			//mTcpClient.sendMessage("bye");
-			mTcpClient.stopClient();
-			conctTask.cancel(true);
-			conctTask = null;
-		}
-		catch (Exception e)
-		{
-			e.printStackTrace();
-		}
-    	super.onDestroy();
+    private class DisconnectButtonListener implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            closeConnection();
+        }
+
     }
-  
-    
+
+    private class ConnectButtonListener implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            initConnection();
+        }
+
+    }
 }
